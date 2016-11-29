613f44da04e114f9f7deb299efc5ffd2211dfa6c
kruczjak <kruczjak@gmail.com>
2016-11-29 21:53:13 +0100

    Response fixes

--- http_server/server.c
+++ http_server/server.c
@@ -298,6 +298,8 @@ void render_gpio(int client) {
     send(client, buffer, strlen(buffer), 0);
     strcpy(buffer, SERVER_NAME);
     send(client, buffer, strlen(buffer), 0);
+    sprintf(buffer, "Content-Type: text/html; charset=UTF-8\r\n");
+    send(client, buffer, strlen(buffer), 0);
 
     buffer[0] = get_simple_state(LED_RED);
     buffer[1] = END_OF_LINE;
@@ -311,6 +313,8 @@ void render_gpio(int client) {
     buffer[1] = END_OF_LINE;
     send(client, buffer, strlen(buffer), 0);
 
+    strcpy(buffer, "\r\n");
+    send(client, buffer, strlen(buffer), 0);
     strcpy(buffer, "\r\n");
     send(client, buffer, strlen(buffer), 0);
 
