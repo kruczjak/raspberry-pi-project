9be2a2f7750a42f457c6b0c138d854c4d8803a83
kruczjak <kruczjak@gmail.com>
2017-01-04 12:07:50 +0100

    Advanced sleep

--- http_server/server.c
+++ http_server/server.c
@@ -6,13 +6,13 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <asm/delay.h>
 #include <sys/wait.h>
 #include <sys/mman.h>
 #include <pthread.h>
 #include <sys/stat.h>
 #include <ctype.h>
 #include <fcntl.h>
+#include <sys/time.h>
 
 #define PORT 80
 #define BACKLOG_SIZE 10 // queue
@@ -102,6 +102,40 @@ void unmap_peripheral(struct bcm2835_peripheral *p) {
     munmap(p->map, BLOCK_SIZE);
     close(p->mem_fd);
 }
+
+//sleeping
+
+void delayMicrosecondsHard (unsigned int howLong)
+{
+    struct timeval tNow, tLong, tEnd ;
+
+    gettimeofday (&tNow, NULL) ;
+    tLong.tv_sec  = howLong / 1000000 ;
+    tLong.tv_usec = howLong % 1000000 ;
+    timeradd (&tNow, &tLong, &tEnd) ;
+
+    while (timercmp (&tNow, &tEnd, <))
+    gettimeofday (&tNow, NULL) ;
+}
+
+void udelay(unsigned int howLong)
+{
+    struct timespec sleeper ;
+    unsigned int uSecs = howLong % 1000000 ;
+    unsigned int wSecs = howLong / 1000000 ;
+
+    /**/ if (howLong ==   0)
+        return ;
+    else if (howLong  < 100)
+        delayMicrosecondsHard (howLong) ;
+    else
+    {
+        sleeper.tv_sec  = wSecs ;
+        sleeper.tv_nsec = (long)(uSecs * 1000L) ;
+        nanosleep (&sleeper, NULL) ;
+    }
+}
+
 /* ############################################################ */
 // input state == 1
 // output state == 0
